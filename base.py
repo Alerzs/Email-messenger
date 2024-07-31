42f12bec7b01a84019310656a80a57763f8e31ef
rozhrafiee <rozhinarafiee014@gmail.com>
2024-07-31 21:41:17 +0430

    rozhs

--- base.py
+++ base.py
@@ -1,35 +1,71 @@
+import hashlib
+import json
+import os
 class Manage:
 
     def __init__(self) -> None:
+        self.load_data()
        #open stored hash data file
-        pass 
-    
+    def load_data(self):
+        if os.path.exists("user.json"):
+            with open("use.json", "r") as f:
+                self.users = json.load(f)
+        else:
+            self.users = {}        
+    def save_data(self):
+        with open("users.json", "w") as f:
+            json.dump(self.users, f)
 
     def creat_hash(self):
+        return hashlib,sha256(password.encode()).hexdigits()
         #use to convert user object to hash
-        pass
     
     def admin_check(self ,user):
+        return user.get("is_admin", False)
         #checks if given user is admin or not
-        pass
 
     def add_new_member(self,email,password):
+        if email in self.users:
+            print("User already exists.")
+            return
+        self,users[email] = {
+            "password": self.create_hash(password),
+            "inbox": [],
+            "is_admin": is_admin
+        }
+        self.save_data()
+        prine(f"User {email} added successfully.")
         #add a member to hash file shold use creat_hash function in it
         #this method is called by admin to add a new member
-        pass
 
     def change_password(self ,user ,new_password):
-        pass
+        self.users[user_email]["password"] = self.create_hash(new_password)
+        self.save_data()
+        print("Password change successfully")
 
     def validation(self ,email ,password) -> bool:
+        if email in self.users and self.users[email]["password"] == self.create_hash(password):
         # checks if data already has a member with given email and password and returns user object if its valid otherwise returns false
-        pass
+            return self.users[email]
+        return False
     
     def send_email(self ,sender_email ,reciver_email ,text):
-        pass
+        if reciver_email not in self.users:
+            print("Reciver not found.")
+            return
+        email = {
+            "sender": sender_mail,
+            "text": text
+        } 
+        self.users[reciver_email][inbox].append(email)
+        self.save_data()
+        print("Email sent successfully.")
+
 
     def find_user(self, email):
-        pass
+        if email in self.users:
+            return self.users[email]
+        return None
 
 class User:
 
